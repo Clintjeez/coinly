302b758c82b2ac9a53e8783c1626b7855a9537cc
clintjeez <clintjeez@gmail.com>
2022-01-06 12:27:07 +0100

    client file structure

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Loader } from "./Loader";
+export { default as Navbar } from "./Loader";
+export { default as Footer } from "./Loader";
+export { default as Services } from "./Loader";
+export { default as Transactions } from "./Loader";
+export { default as Welcome } from "./Loader";
