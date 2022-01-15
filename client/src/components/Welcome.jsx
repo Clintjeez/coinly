982d75e50ff6a7dc5fc8bffdba476970d8db1e85
clintjeez <clintjeez@gmail.com>
2022-01-16 00:33:55 +0100

    blockchain transaction setup

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
+
+import { TransactionContext } from "../context/TransactionContext";
 
 import { Loader } from "./";
 
@@ -21,8 +23,21 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const connectWallet = () => {};
-  const handleSubmit = () => {};
+  const {
+    connectWallet,
+    currentAccount,
+    formData,
+    sendTransaction,
+    handleChange,
+  } = useContext(TransactionContext);
+
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+    console.log(formData);
+    e.preventDefault();
+    if (!addressTo || !amount || !keyword || !message) return;
+    sendTransaction();
+  };
 
   return (
     <div className="flex w-full justify-center items-center">
@@ -35,19 +50,21 @@ const Welcome = () => {
             Explore the crypto world. Buy and Sell cryptocurrencies easily on
             coinly.com{" "}
           </p>
-          <button
-            type="button"
-            onClick={connectWallet}
-            className="flex flex-row justify-center 
+          {!currentAccount && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="flex flex-row justify-center 
           items-center my-5 bg-[#2952e3] p-3 
           rounded-full cursor-pointer 
           hover:bg-[#2546bd]"
-          >
-            <p className="text-white text-base font-semibold">
-              {" "}
-              Connect Wallet
-            </p>
-          </button>
+            >
+              <p className="text-white text-base font-semibold">
+                {" "}
+                Connect Wallet
+              </p>
+            </button>
+          )}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
@@ -90,25 +107,25 @@ const Welcome = () => {
               placeholder="Address To"
               name="addressTo"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Amount (ETH)"
               name="amount"
               type="number"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Keyword (Gif)"
               name="keyword"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Enter Message"
               name="message"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
